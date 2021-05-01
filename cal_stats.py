e1ee8dc9b6effdb4c568ccfe394963b10d18da60
Duy Vu <1205vuduy2001@gmail.com>
2021-05-01 11:51:14 +0300

    Add ratio cal

--- cal_stats.py
+++ cal_stats.py
@@ -5,16 +5,26 @@ import matplotlib.pyplot as plt
 if __name__ == "__main__":
     file_name = sys.argv[-1]
     df = pd.read_csv(file_name, index_col=0)
-    print(df)
+    print(df, end='\n\n')
 
-    unique_frame = df.drop_duplicates("world_index")
     plt.figure(figsize=(25, 6), dpi=80)
-    plt.scatter(unique_frame["world_index"], unique_frame['is_baby'])
+    unique_frames_df = df.drop_duplicates("world_index")
+    total_unique_frames = unique_frames_df['world_index'].nunique()
+    plt.scatter(unique_frames_df["world_index"], unique_frames_df['is_baby'])
+    # plt.show()
+
     detected_baby_df = df[df['is_baby'] == True]
-    print("Total frames having baby", detected_baby_df['world_index'].nunique())
+    total__detected_frames = detected_baby_df['world_index'].nunique()
+    print(f"Total frames having baby {total__detected_frames}, i.e {total__detected_frames/total_unique_frames*100:.2f}%")
 
-    gaze_in_box = detected_baby_df[detected_baby_df['in_bounding_box']==True]
-    print("Loooking at box of baby", gaze_in_box['world_index'].nunique())
+    gaze_in_box_df = detected_baby_df[detected_baby_df['in_bounding_box']==True]
+    gaze_in_box = gaze_in_box_df['world_index'].nunique()
+    print(f"Loooking at baby (in bounding box): {gaze_in_box}, i.e {gaze_in_box/total__detected_frames*100:.2f}%")
 
-    gaze_in_seg = detected_baby_df[detected_baby_df['in_segmentation']==True]
-    print("Loooking at segmentation of baby", gaze_in_seg['world_index'].nunique())
+    gaze_in_seg_df = detected_baby_df[detected_baby_df['in_segmentation']==True]
+    gaze_in_seg = gaze_in_seg_df['world_index'].nunique()
+    print(f"Loooking at baby (in segmentation): {gaze_in_seg}, i.e {gaze_in_seg/total__detected_frames*100:.2f}%")
+
+
+
+    
