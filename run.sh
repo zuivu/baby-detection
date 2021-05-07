43d5d486fda715c886d0477699db3214e65c828f
Zui <38757448+Duy-Vu@users.noreply.github.com>
2021-05-07 15:30:06 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,15 +2,17 @@
 
 #SBATCH -J "baby-detection"
 #SBATCH -o logs/logs_%A.txt
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks=1
 #SBATCH --mem-per-cpu=1G
-#SBATCH --nodes 2
-#SBATCH --partition gpu
-#SBATCH --gres=gpu:teslav100:2
+#SBATCh --cpus-per-task=10
 #SBATCH --mail-user=<your_email_address>
 #SBATCH --mail-type=ALL
-#SBATCH --time=01:30:00
+#SBATCH --time=03:00:00
 
 export PYTHONPATH=$PYTHONPATH:.
 
 module load CUDA
-python -u extract_world_view.py <recording_dir>
+source activate <env_name>
+python extract_world_view.py <recording_dir>
