dcada904efb00fbb6fa1cf0d5e091c325bf86a87
Duy Vu <1205vuduy2001@gmail.com>
2021-05-07 15:53:34 +0300

    Add plot

--- cal_stats.py
+++ cal_stats.py
@@ -7,24 +7,38 @@ if __name__ == "__main__":
     df = pd.read_csv(file_name, index_col=0)
     print(df, end='\n\n')
 
-    plt.figure(figsize=(25, 6), dpi=80)
-    unique_frames_df = df.drop_duplicates("world_index")
-    total_unique_frames = unique_frames_df['world_index'].nunique()
-    plt.scatter(unique_frames_df["world_index"], unique_frames_df['is_baby'])
-    # plt.show()
-
+    # Stats
     detected_baby_df = df[df['is_baby'] == True]
-    total__detected_frames = detected_baby_df['world_index'].nunique()
-    print(f"Total frames having baby {total__detected_frames}, i.e {total__detected_frames/total_unique_frames*100:.2f}%")
+    total_detected_frames = detected_baby_df['world_index'].nunique()
+    total_unique_frames = df['world_index'].nunique()
+    print(f"Total frames having baby {total_detected_frames}, i.e {total_detected_frames/total_unique_frames*100:.2f}%")
 
     gaze_in_box_df = detected_baby_df[detected_baby_df['in_bounding_box']==True]
     gaze_in_box = gaze_in_box_df['world_index'].nunique()
-    print(f"Loooking at baby (in bounding box): {gaze_in_box}, i.e {gaze_in_box/total__detected_frames*100:.2f}%")
-
+    print(f"Loooking at baby (in bounding box): {gaze_in_box}, i.e {gaze_in_box/total_detected_frames*100:.2f}%")
+        
     gaze_in_seg_df = detected_baby_df[detected_baby_df['in_segmentation']==True]
     gaze_in_seg = gaze_in_seg_df['world_index'].nunique()
-    print(f"Loooking at baby (in segmentation): {gaze_in_seg}, i.e {gaze_in_seg/total__detected_frames*100:.2f}%")
+    print(f"Loooking at baby (in segmentation): {gaze_in_seg}, i.e {gaze_in_seg/total_detected_frames*100:.2f}%")
 
+    # Plot
+    df = df.drop_duplicates("world_index")
+    yes_baby = df[df['is_baby']==True]
+    no_baby = df[df['is_baby']==False]
+    gaze_in_box_df = gaze_in_box_df.drop_duplicates(subset=['world_index'])
+    gaze_in_seg_df = gaze_in_seg_df.drop_duplicates(subset=['world_index'])
+
+    plt.figure(figsize=(10, 3), dpi=120)
+    plt.scatter(yes_baby["world_index"], yes_baby['is_baby'], marker='.', label='yes baby')
+    plt.scatter(no_baby["world_index"], no_baby['is_baby'], marker='.', label='no baby')
+    plt.scatter(gaze_in_box_df['world_index'], gaze_in_box_df['in_bounding_box'], c='r', marker=2, alpha=0.4, label='gaze in box')
+    plt.scatter(gaze_in_seg_df['world_index'], gaze_in_seg_df['in_segmentation'], c='g', marker=3, alpha=0.5, label='gaze segmentation')
+    plt.xticks(range(0, 1171, 90))
+    plt.yticks([0, 1], labels=['NO BABY', 'YES BABY'])
+    plt.legend(loc='best')
+    plt.grid()
+    plt.tight_layout()
+    plt.show()
 
 
     
